2f5af75a74f9fc9262cf3c8ea17aa54298d987b2
erickson <reinosoe@uninorte.edu.co>
2021-09-20 21:15:06 -0500

    Se agrego el animal paloma y su tipo de vertebrado

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -4,6 +4,7 @@ public class GeneradorVertebrados {
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero");
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
         miVertebrados[2] = new Vertebrado("Perro", "Mamifero");
+        miVertebrados[3] = new Vertebrado("Paloma", "Ave");
 
         return miVertebrados;
     }
