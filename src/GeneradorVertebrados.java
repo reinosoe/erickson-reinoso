b331e986fec49cb6f5e3b63f2ce82329f678507a
erickson <reinosoe@uninorte.edu.co>
2021-09-20 21:28:08 -0500

    cambio indice

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -3,8 +3,8 @@ public class GeneradorVertebrados {
         Vertebrado[] miVertebrados = new Vertebrado[4];
         miVertebrados[0] = new Vertebrado("Camello", "Mamifero");
         miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
-        miVertebrados[2] = new Vertebrado("Perro", "Mamifero");
-        miVertebrados[3] = new Vertebrado("Paloma", "Ave");
+        miVertebrados[3] = new Vertebrado("Perro", "Mamifero");
+        miVertebrados[2] = new Vertebrado("Paloma", "Ave");
 
         return miVertebrados;
     }
